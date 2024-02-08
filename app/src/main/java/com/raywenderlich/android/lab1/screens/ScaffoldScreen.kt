d4ce5658c8de5985b8a1308e4838112f3570fabb
Vadim <zyuskovv@gmail.com>
2024-02-08 13:31:46 +0300

    MyBottomAppBar

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
@@ -73,5 +73,7 @@ fun MyTopAppBar(scaffoldState: ScaffoldState, scope: CoroutineScope){
 
 @Composable
 fun MyBottomAppBar(){
-
+    BottomAppBar (
+    content = {},
+    backgroundColor = colorResource(id = R.color.colorPrimary))
 }
